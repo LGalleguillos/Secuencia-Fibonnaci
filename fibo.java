e741ed4dbbfd40fb6c1420db7c176c26f6ccfcaa
LGalleguillos <44623233+LGalleguillos@users.noreply.github.com>
2018-11-14 19:28:11 -0300

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -15,15 +15,18 @@ public class fibo {
 		Scanner S=new Scanner(System.in);
 		System.out.println("ingrese la cantidad de numeros fibonacci");
 		N=S.nextInt();
+		//caso 0
 		if(N==0){
 			System.out.println("ingrese un numero valido");
 		}
 		else{
+			//caso 1
 			if(N==1){
 				System.out.println("la secuencia es:");
 				System.out.println(z);
 			}
 			else{
+				//los demas numeros
 				if(N==2){
 					System.out.println("la secuencia es:");
 					System.out.println(z+a);
